3024b1675d4482bcc9e1e511983ee1333408cf07
chaabaoussama <chaabaoussama233@gmail.com>
2024-11-20 17:58:24 +0100

    TVA

--- index.js
+++ index.js
@@ -170,5 +170,5 @@ app.get('/uploads/:filename', (req, res) => {
 });
 
 // Démarrage du serveur
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
